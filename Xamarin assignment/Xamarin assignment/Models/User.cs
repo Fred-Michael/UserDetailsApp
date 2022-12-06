8bb5fa0f02a6a2eec3c6cc46a3fc0eb9617588a2
Fr3dd <chinazor.okafor@direct.id>
2022-12-06 13:14:15 -0800

    code cleanup

--- Xamarin assignment/Xamarin assignment/Models/User.cs
+++ Xamarin assignment/Xamarin assignment/Models/User.cs
@@ -1,6 +1,6 @@
 ﻿using SQLite;
 using System.ComponentModel.DataAnnotations;
-using MaxLengthAttribute = System.ComponentModel.DataAnnotations.MaxLengthAttribute;
+using MaxLengthAttribute = SQLite.MaxLengthAttribute;
 
 namespace Xamarin_assignment.Models
 {
@@ -8,15 +8,14 @@ namespace Xamarin_assignment.Models
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
         [MaxLength(150)]
         public string Name { get; set; }
 
-        [MaxLength(1)]
         public string Sex { get; set; }
         public string Address { get; set; }
 
         [Phone]
         public string PhoneNumber { get; set; }
-        //public byte Picture { get; set; }
     }
 }
--- Xamarin assignment/Xamarin assignment/Views/UserDetails.xaml.cs
+++ Xamarin assignment/Xamarin assignment/Views/UserDetails.xaml.cs
@@ -1,6 +1,4 @@
-﻿using SQLite;
-using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Xamarin_assignment.Models;
 using Xamarin_assignment.ViewModels;
@@ -14,63 +12,7 @@ namespace Xamarin_assignment.Views
         public UserDetails()
         {
             InitializeComponent();
-            //_selectedUser = selectedUser;
-            //SetFields(_selectedUser);
             BindingContext = new UserDetailsVM(_selectedUser);
-        }
-
-        //private async void UpdateUserButton_Clicked(object sender, EventArgs e)
-        //{
-        //    var updatedUser = UpdateFields();
-        //    using (SQLiteConnection conn = new SQLiteConnection(App.databaseLocation))
-        //    {
-        //        var choice = await DisplayAlert(
-        //            "Update User",
-        //            $"Are you sure you want to update this record?",
-        //            "Yes",
-        //            "No");
-
-        //        if (choice)
-        //        {
-        //            conn.Update(updatedUser);
-        //            await Navigation.PopToRootAsync();
-        //        }
-        //    }
-        //}
-
-        private async void DeleteUserButton_Clicked(object sender, EventArgs e)
-        {
-            using (SQLiteConnection conn = new SQLiteConnection(App.databaseLocation))
-            {
-                var choice = await DisplayAlert(
-                    "Delete User",
-                    $"Are you sure you want to delete {_selectedUser.Name}?",
-                    "Yes",
-                    "No");
-
-                if (choice)
-                {
-                    conn.Delete(_selectedUser);
-                    await Navigation.PopToRootAsync();
-                }
-            }
-        }
-
-        //private void SetFields(User user)
-        //{
-        //    update_user_name.Text = user.Name;
-        //    update_user_address.Text = user.Address;
-        //    update_user_sex.Text = user.Sex;
-        //    update_user_number.Text = user.PhoneNumber;
-        //}
-
-        private User UpdateFields()
-        {
-            _selectedUser.Name = update_user_name.Text;
-            _selectedUser.Address = update_user_address.Text;
-            //_selectedUser.Sex = update_user_sex.Text;
-            _selectedUser.PhoneNumber = update_user_number.Text;
-            return _selectedUser;
         }
     }
 }
